ef43cc010075698ed5110e34f57abde1972b0b06
Jinghong Gao <133833687+gaojh135@users.noreply.github.com>
2025-05-08 18:13:05 +0800

    Create upsamplelayer.py

---
+++ nnUNet/nnunetv2/nets/building_blocks/upsamplelayer.py
@@ -0,0 +1,21 @@
+from torch import nn
+from torch.nn import functional as F
+
+class UpsampleLayer(nn.Module):
+    def __init__(
+            self,
+            conv_op,
+            input_channels,
+            output_channels,
+            pool_op_kernel_size,
+            mode='nearest'
+        ):
+        super().__init__()
+        self.conv = conv_op(input_channels, output_channels, kernel_size=1)
+        self.pool_op_kernel_size = pool_op_kernel_size
+        self.mode = mode
+        
+    def forward(self, x):
+        x = F.interpolate(x, scale_factor=self.pool_op_kernel_size, mode=self.mode)
+        x = self.conv(x)
+        return x
